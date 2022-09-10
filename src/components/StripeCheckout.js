d92924e1d347573476323169c510e3e83f7793d5
Draav Jain <draavjain2001@gmail.com>
2022-09-11 00:24:20 +0530

    initial commit

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -53,7 +53,6 @@ const CheckoutForm = () => {
         "/.netlify/functions/create-payment-intent",
         JSON.stringify({ cart, shipping_fee, total_amount })
       );
-      console.log(data.clientSecret);
       setClientSecret(data.clientSecret);
     } catch (error) {}
   };
